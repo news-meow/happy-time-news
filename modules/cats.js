7f5487044dc2fa5fc4882eb0dcfdade730fc506f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-21 12:36:31 -0500

    Cat API request will receive through Postman test. Believe request to be complete.

---
+++ modules/cats.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const superagent =require('superagent');
+// const client = require ('../utilities.database');
+// const errorHandler = require('./error');
+
+// cats route handler
+function getCatsFromApi (request, response) {
+    const url = 'https://api.thecatapi.com/v1/images/search?mime_types=gif';
+    return superagent.get(url)
+        .set('Authorization', `x-api-key ${process.env.CATS_API}`)
+        .then (catsResponse => {
+            const catsData = catsResponse.body;
+            const catGif = () => new Cat(catsData)
+            response.render('pages/index', { cat: catGif})
+        })
+        .catch (err => {
+            console.error(err);
+            next(err);
+        });
+}
+
+function Cat(catsData) {
+    this.url = catsData.url;
+}
+
+module.exports = getCatsFromApi;
