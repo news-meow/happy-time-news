4510c585005b1ed11473e3b90f7747e025c0a941
cnickels21 <cnickels_21@yahoo.com>
2020-04-21 11:35:36 -0500

    actual schema structure

--- schema.sql
+++ schema.sql
@@ -3,5 +3,9 @@ DROP TABLE IF EXISTS articles;
 CREATE TABLE IF NOT EXISTS articles (
     id SERIAL PRIMARY KEY,
     title VARCHAR (50),
-    author VARCHAR (50)
+    author VARCHAR (50),
+    source VARCHAR (50),
+    url VARCHAR (1000),
+    image_url VARCHAR (1000),
+    description VARCHAR (2000)
 );
