948557816248e97cc77d8d48a4b03420a42fe2ad
cnickels21 <cnickels_21@yahoo.com>
2020-04-23 10:11:42 -0500

    no more duplicates in DB

--- modules/catalog.js
+++ modules/catalog.js
@@ -7,19 +7,37 @@ const client = require('./db');
 // putting stuff in SQL database
 function setArticlesToDB(request, response) {
   let newArticle = request.body;
-  const SQL = `INSERT INTO articles (title, author, source, url, image_url, description) VALUES ($1, $2, $3, $4, $5, $6)`;
-  const parameters = [newArticle.title, newArticle.author, newArticle.source, newArticle.url, newArticle.image_url, newArticle.description];
 
-  client.query(SQL, parameters)
-    .then(result => {
-      response.redirect('/');
+  const verifySQL = `
+    SELECT * FROM articles
+    WHERE title = $1
+  `;
+  const verifyExistence = [newArticle.title];
+
+  client.query(verifySQL, verifyExistence)
+    .then(verifiedResult => {
+      if (verifiedResult.rowCount === 0) {
+        const SQL = `
+        INSERT INTO articles (title, author, source, url, image_url, description)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING id`;
+        const parameters = [newArticle.title, newArticle.author, newArticle.source, newArticle.url, newArticle.image_url, newArticle.description];
+        return client.query(SQL, parameters)
+          .then(result => {
+            console.log('Article saved', result);
+            let id = result.rows[0].id;
+            response.redirect('/');
+          })
+      } else {
+        console.log('You already saved this!')
+        let id = verifiedResult.rows[0].id;
+        response.redirect('/');
+      }
     })
     .catch(err => {
       errorHandler(err, request, response);
     });
 }
-
-
 
 function getArticlesFromDB(request, response) {
   const SQL = 'SELECT * FROM articles;';
