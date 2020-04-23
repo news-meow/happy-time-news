69eb79476afc03cfe102fbb9f92cd98e6a986847
cnickels21 <cnickels_21@yahoo.com>
2020-04-23 14:07:04 -0500

    server js organized

--- server.js
+++ server.js
@@ -1,52 +1,38 @@
 'use strict';
 
 // Dependencies
-
 require('dotenv').config();
-
 const express = require('express');
-// const superagent = require('superagent');
 const pg = require('pg');
 const methodOverride = require('method-override');
-const app = express();
-const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-const homePageRouteHandler = require('./modules/news');
-const errorHandler = require('./modules/error');
-const catalogModule = require('./modules/catalog');
-const client = require('./modules/db');
 
-const { setArticlesToDB, getArticlesFromDB } = catalogModule;
-
-
-const deleteArticlesFromDB = require('./modules/delete')
-
-// Connected to SQL database
-
-
-
-
-
+// App setup
+const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+// Modules required
+const homePageRouteHandler = require('./modules/news');
+const errorHandler = require('./modules/error');
+const client = require('./modules/db');
+const catalogModule = require('./modules/catalog');
+const { setArticlesToDB, getArticlesFromDB } = catalogModule;
+const deleteArticlesFromDB = require('./modules/delete')
 
 // Routes
-
 app.get('/', homePageRouteHandler);
+app.get('/catalog', getArticlesFromDB);
 app.get('/about', (request, response) => {
   response.render('pages/about');
 });
-app.get('/catalog', getArticlesFromDB);
 
 app.post('/save', setArticlesToDB);
-
 app.delete('/articles/:id', deleteArticlesFromDB);
-
-
-
 
 app.get('*', function(request, response, next) {
   let err = new Error(`${request.ip} tried to reach ${request.originalUrl}`);
